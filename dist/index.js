0298593fef34b7613fccef17bcc2cb9e6cc3e763
AlexandreLeonetti <al.leo@wanadoo.fr>
2024-04-27 11:51:23 +0200

    merge

---
+++ dist/index.js
@@ -0,0 +1,38 @@
+let a1 = [-4, -2, 0, 2, 4, 6, 8, 10];
+let a2 = [-7, -6, -5, -4, -3, -2, -1];
+let a3 = [0, 5, 10, 15, 20, 25, 30, 1000];
+function arraySortedMerge(arrs) {
+    let res = [];
+    /* build some array of iterator */
+    let iters = arrs.map(numArr => numArr[Symbol.iterator]());
+    /* build a heap using an array and the sort method */
+    let heap = [];
+    iters.forEach((iter, idx) => {
+        let tuple = [];
+        tuple.push(iter.next().value, idx);
+        heap.push(tuple);
+        heap.sort((a, b) => a[0] - b[0]);
+    });
+    //console.log(heap);
+    /* build a loop that pushes all elements until nothing is left through the heap */
+    while (heap.length > 0) {
+        /* push smallest elt from heap into res */
+        console.log(heap);
+        let minIdx;
+        let tuple = heap.shift();
+        if (tuple !== undefined) {
+            res.push(tuple[0]);
+            minIdx = tuple[1];
+            let nextVal = iters[minIdx].next().value;
+            if (nextVal !== undefined) {
+                heap.push([nextVal, minIdx]);
+                heap.sort((a, b) => a[0] - b[0]);
+            }
+        }
+    }
+    console.log(res);
+    return res;
+}
+;
+arraySortedMerge([a1, a2, a3]);
+export {};
---
+++ readme.md
@@ -0,0 +1,26 @@
+# merge n sorted arrays using TypeScript 
+
+> merge n sorted arrays using TypeScript. 
+
+## Install
+
+```sh
+
+npm install @leonetti/array-sorted-merge
+
+```
+
+## Usage
+
+```js
+
+import {arraySortedMerge} from '@leonetti/array-sorted-merge';
+
+let a1 = [-4,-2,0,2,4,6,8,10];
+let a2 = [-7,-6,-5,-4,-3,-2,-1];
+let a3 = [0,5,10,15,20,25,30,1000];
+
+arraySortedMerge([a1,a2,a3])   // 
+
+
+```
